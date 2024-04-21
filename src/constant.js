b97f5d3ebdd6a3ea46c75d197656d9bf1cf42275
ManojDhaka57 <manojdhaka7599@gmail.com>
2024-04-21 12:37:06 +0530

    udpate constant and cloudinary

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,1 @@
-export const DB_NAME = 'videoStreaming';
+export const DB_NAME = "videoStreaming";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
-connectDB();
+  path: "./env",
+});
+const port = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server listening on port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDb connection error: " + error);
+  });
